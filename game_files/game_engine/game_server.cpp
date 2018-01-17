1de256dd3757721d10a78f24198daae74e34060b
Unknown <mmurawski777@gmail.com>
2018-01-17 20:50:29 +0100

    1: Auto stash before checking out "branch2"

--- game_files/game_engine/game_server.cpp
+++ game_files/game_engine/game_server.cpp
@@ -1,47 +1,19 @@
 #include "game_server.hpp"
+#include "../get_regex_function.hpp"
 
+
+//Deklaracja pól satycznych
 tcp::tcp_server GameServer::_server;
 std::vector<std::shared_ptr<tcp::tcp_client>> GameServer::_clients_ptr;
 long unsigned int GameServer::_buffer_length=3;
 std::queue<std::string> GameServer::_queue_requests;
 std::queue<std::string> GameServer::_queue_responses;
 std::vector<std::shared_ptr<GameEngine>> GameServer::_engines;
+std::mutex GameServer::m_;
 bool GameServer::_is_running=true;
 
+
 //funkcje pomocnicze
-std::string getPort(std::string &request){
-  boost::regex rgxPort("(?<=<port>)(.*)(?=</port>)");
-  boost::smatch matchPort;
-  if(boost::regex_search(request,matchPort ,rgxPort)){
-    return matchPort[0];
-  }else{
-    return "";
-  }
-  
-}
-
-std::string getIp(std::string &request){
-  boost::regex rgxIp("(?<=<ip>)(.*)(?=</ip>)");
-  boost::smatch matchIp;
-if(boost::regex_search(request,matchIp ,rgxIp)){
-    return matchIp[0];
-  }else{
-    return "";
-  }
-}
-
-std::string getName(std::string &request){
-  boost::regex rgxIp("(?<=<name>)(.*)(?=</name>)");
-
-  boost::smatch matchIp;
-if(boost::regex_search(request,matchIp ,rgxIp)){
-    return matchIp[0];
-  }else{
-    return "";
-  }
-}
-
-
 bool gameExist(std::string name,const std::vector<std::shared_ptr<GameEngine>>& engines){
   for(std::shared_ptr<GameEngine> engine_ptr : engines){
     if(engine_ptr.get()->_name==name){
@@ -51,7 +23,33 @@ bool gameExist(std::string name,const std::vector<std::shared_ptr<GameEngine>>& 
       return false;
 }
 
+std::shared_ptr <GameEngine> GameServer::findGameEngine(std::string name){
+  std::vector<std::shared_ptr<GameEngine>>::iterator it0 = find_if(_engines.begin(), _engines.end(),
+     [&name] (const std::shared_ptr<GameEngine>& engine) { return engine->_name == name; });
+     if(it0!=_engines.end()){
+       return *it0;
+     }else{
+       return nullptr;
+     }
+}
+
+std::shared_ptr<tcp::tcp_client> GameServer::findTCPClient(std::string host,std::string port){
+  std::vector<std::shared_ptr<tcp::tcp_client>>::iterator it1 = 
+  find_if(_clients_ptr.begin(), _clients_ptr.end(),
+  [&host,&port] (const std::shared_ptr<tcp::tcp_client>& _client_ptr){
+  return _client_ptr->get_host() == host && _client_ptr->get_port() == atoi(port.c_str());
+         });
+
+    if(it1!=_clients_ptr.end()){
+      return *it1;
+    }else{
+      return nullptr;
+    }
+}
+
+
 GameServer::GameServer(){
+  std::cout<<"Utworzono server"<<std::endl;
   _thread_requests = new std::thread(&GameServer::process_queue_request, this);
   _thread_responses = new std::thread(&GameServer::process_queue_response, this);
 }
@@ -68,7 +66,7 @@ GameServer::~GameServer(){
         }
 }
 
-GameServer & GameServer::get(){
+GameServer & GameServer::getInstance(){
         static GameServer gameServer;
         return gameServer;
 }
@@ -118,11 +116,11 @@ void GameServer::data_collector(const std::shared_ptr<tcp::tcp_client>& client,c
 
 void GameServer::start(const std::string& host="127.0.0.1", std::uint32_t port=3002){
     _server.start(host, port, [&](const std::shared_ptr<tcp::tcp_client>& client) -> bool {
-    std::cout << "Nowy klient dołączył do gry " <<client->get_host()<< std::endl;
+    std::cout << "Nowy klient dołączył do serwera" <<client->get_host()<< std::endl;
     GameServer::_clients_ptr.push_back(client);
-    for(std::shared_ptr<tcp::tcp_client> i : _clients_ptr){
-      std::cout<<i->get_host()<<i->get_port()<<std::endl;
-    }
+    std::string lista = getLobby();
+    std::vector<char> listaGier(lista.begin(), lista.end());
+    client->async_write({listaGier, nullptr});
     client->async_read({_buffer_length, std::bind(&data_collector, client, std::placeholders::_1)});
     return true;
   });
@@ -134,60 +132,52 @@ while(_is_running){
 //przetwarzanie danych z kolejki otrzymanych danych
 std::string request = _queue_requests.front();
 _queue_requests.pop();
-boost::regex rgxJoin("(?<=<join>)(.*)(?=</join>)");
-boost::smatch matchJoin;
-boost::regex rgxCreate("(?<=<create>)(.*)(?=</create>)");
-boost::smatch matchCreate;
+std::string join = getJoin(request);
+std::string create = getCreate(request);
 //odczytanie "adresata"
 std::string host = getIp(request);
 std::string port = getPort(request);
 std::string name = getName(request);
-
-if(boost::regex_search(request,matchCreate ,rgxCreate)){
-//  //sprawdzanie czy gra instnieje
-  if(gameExist(matchCreate[0],_engines)){
+std::cout<<host<<port<<name<<"join="<<join<<"create="<<create<<std::endl;
+if(join!=""){
+  std::cout<<"JOIN!"<<std::endl;
+  std::shared_ptr<GameEngine> engine_ptr = findGameEngine(join);
+  std::shared_ptr<tcp::tcp_client> client_ptr = findTCPClient(host,port);
+  // sprawdzenie czy gra istnieje
+  if(engine_ptr==nullptr){
+    std::cout<<"engine nullptr!"<<std::endl;
+    //gra nie istnieje byebye
     _queue_responses.push("<ip>"+host+"</ip><port>"+port+
     "</port><msg>Gra o podanej nazwie istnieje. Wybierz innną nazwę</msg>");
-  }else{
-    _queue_responses.push("<ip>"+host+"</ip><port>"+port+"</port><msg>Utworzono grę</msg>");
-//  //utworzenie gry
-    
-    //narusza pamięć       
-    std::vector<std::shared_ptr<tcp::tcp_client>>::iterator it1= std::find_if(_clients_ptr.begin(), _clients_ptr.end(), 
-    [&host,&port](std::shared_ptr<tcp::tcp_client> const& i){ return i.get()->get_host() == host &&
-    i.get()->get_port()==atoi(port.c_str()); });
-  
-    std::vector<std::shared_ptr<GameEngine>>::iterator it2= std::find_if(_engines.begin(), _engines.end(), 
-    [&matchCreate](std::shared_ptr<GameEngine> const& i){ return i.get()->_name == matchCreate[0]; });
-    // naruszenie
-    _engines.push_back(std::make_shared<GameEngine>(matchCreate[0],name,*it1));
-    std::cout<<"WYPISYWANIE KLIENTOW"<<std::endl;
-    _engines.back()->_queue.push("HANDSHAKE");
-    std::cout<<"WYPISYWANIE GIER"<<std::endl;
-    for(std::shared_ptr<GameEngine> engine : _engines) std::cout<<"name: "<<engine.get()->_name<<std::endl;
-
+  }else if(engine_ptr!=nullptr){
+    std::cout<<"DODAWANIE"<<std::endl;
+    //gra istnieje dołączanie gracza
+      _queue_responses.push("<ip>"+host+"</ip><port>"+port+"</port><msg>Dołączonono do gry</msg>");
+      engine_ptr.get()->addPlayer(name,client_ptr);
+      engine_ptr.get()->_queue.push("HANDSHAKE");
+   // 
   }
 }
-    if(boost::regex_search(request,matchJoin ,rgxJoin)){
-      std::cout<<"join"<<std::endl;
-      for(std::shared_ptr<GameEngine> engine : _engines) std::cout<<"name: "<<engine.get()->_name<<std::endl;
-      std::vector<std::shared_ptr<GameEngine>>::iterator it0 = find_if(_engines.begin(), _engines.end(),
-     [&matchJoin] (const std::shared_ptr<GameEngine>& engine) { return engine->_name == matchJoin[0]; });
+if(create!=""){
+  std::cout<<"create!"<<std::endl;
+  std::shared_ptr<GameEngine> engine_ptr = findGameEngine(create);
+  std::shared_ptr<tcp::tcp_client> client_ptr = findTCPClient(host,port);
+  //sprawdzenie czy gra istnieje
+  if(engine_ptr!=nullptr){
+    //gra istnieje byebye
+    std::cout<<"Gra nie istnieje"<<std::endl;
+    _queue_responses.push("<ip>"+host+"</ip><port>"+port+"</port><msg>Gra o podanej nazwie nie istnieje. Wybierz innną nazwę</msg>"); 
+  }else{
+    std::cout<<"tworzenie nowje!"<<std::endl;
+    //Utworzenie nnowej gry
+    std::shared_ptr<tcp::tcp_client> client_ptr = findTCPClient(host,port);
+    std::shared_ptr<GameEngine> engine_ptr = findGameEngine(create);
+    _engines.push_back(std::make_shared<GameEngine>(create,name,client_ptr));
+    _engines.back()->_queue.push("HANDSHAKE");
+    _queue_responses.push("<ip>"+host+"</ip><port>"+port+"</port><msg>Utworzono grę</msg>");
+    }
+}
 
-  std::vector<std::shared_ptr<tcp::tcp_client>>::iterator it1 = find_if(_clients_ptr.begin(), _clients_ptr.end(),
-     [&host,&port] (const std::shared_ptr<tcp::tcp_client>& _client_ptr) {
-        return _client_ptr->get_host() == host && _client_ptr->get_port() == atoi(port.c_str());
-         });
-     if(it0 != _engines.end()){
-
-       (*it0)->addPlayer(name,*it1);
-       (*it0)->_queue.push("HANDSHAKE");
-       _queue_responses.push("<ip>"+host+"</ip><port>"+port+"</port><msg>Dołączonono do gry</msg>");
-     }else{
-       std::cout<<"Gra nie istnieje"<<std::endl;
-      _queue_responses.push("<ip>"+host+"</ip><port>"+port+"</port><msg>Gra o podanej nazwie nie istnieje. Wybierz innną nazwę</msg>");
-     }
-    }else{}
    }
    }
 }
@@ -195,7 +185,6 @@ if(boost::regex_search(request,matchCreate ,rgxCreate)){
 void GameServer::process_queue_response(){
   //przesyłanie odpowiedzi do klientów
   while(_is_running){
-    
     while(!_queue_responses.empty()){
         std::string response = _queue_responses.front();
         _queue_responses.pop();
@@ -203,15 +192,12 @@ void GameServer::process_queue_response(){
         std::string host = getIp(response);
         std::string port = getPort(response);
         std::cout<<":"<<host<<":"<<port;//<<atoi(port.c_str())<<":"<<std::endl;
-        std::vector<std::shared_ptr<tcp::tcp_client>>::iterator it1 = find_if(_clients_ptr.begin(), _clients_ptr.end(),
-     [&host,&port] (const std::shared_ptr<tcp::tcp_client>& _client_ptr) {
-        return _client_ptr->get_host() == host && _client_ptr->get_port() == atoi(port.c_str());
-         });
+        std::shared_ptr<tcp::tcp_client> client_ptr = findTCPClient(host,port);
         for(std::shared_ptr<tcp::tcp_client> client_ptr : _clients_ptr) std::cout<<client_ptr.get()->get_host()<<client_ptr.get()->get_port()<<std::endl;
-        if(it1 !=_clients_ptr.end()){
-          std::cout<<"Do klienta"<<(*it1)->get_host()<<std::endl;
+        if(client_ptr !=nullptr){
+          std::cout<<"Do klienta"<<client_ptr->get_host()<<std::endl;
           std::vector<char> sendable(response.begin(), response.end());
-          (*it1)->async_write({sendable, nullptr});
+          client_ptr->async_write({sendable, nullptr});
         }else{
           std::cout<<"nie znaleziono klienta"<<std::endl;
         }
@@ -222,6 +208,12 @@ void GameServer::process_queue_response(){
 
 void GameServer::queue_result(std::string result){
   _queue_responses.push(result);
+}
+
+std::string GameServer::getLobby(){
+  std::string result="NAZWA GRY\n";
+  if(_engines.size()>0)for(std::shared_ptr<GameEngine> engine_ptr : _engines) result+=engine_ptr.get()->_name+"\n";
+  return result;
 }
 
 
